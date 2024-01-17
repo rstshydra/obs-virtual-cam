3d4e68b42f75a0a7a25ea1a1c40c84d9c8c455c6
rstshydra <118101980+rstshydra@users.noreply.github.com>
2024-01-17 23:57:41 +0000

    resolution change

--- src/virtual-source/virtual-cam.cpp
+++ src/virtual-source/virtual-cam.cpp
@@ -241,6 +241,7 @@ bool CVCamStream::ListSupportFormat()
 	if (format_list.size() > 0)
 		format_list.clear();
 
+	format_list.push_back(struct format(720, 960, 333333));
 	format_list.push_back(struct format(1920, 1080, 333333));
 	format_list.push_back(struct format(1280, 720, 333333));
 	format_list.push_back(struct format(960, 540, 333333));
